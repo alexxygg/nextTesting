ffb6280be2761644b8000a364d5f8130cc7bd380
alexxygg <alexxygg@gmail.com>
2023-03-03 21:13:28 -0800

    Update [id].js

--- next-testing/src/pages/accounts/[id].js
+++ next-testing/src/pages/accounts/[id].js
@@ -1,6 +1,5 @@
 import Accounts from "@/components/Accounts";
 import Footer from "@/components/Footer";
-import UserSigned from "@/components/UserSigned";
 import Login from "@/components/Login";
 
 import { supabase } from "lib/supabaseClient";
