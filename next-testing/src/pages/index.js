b706fce83d3a609a1ceed34d81167994a72b0f9a
alexxygg <alexxygg@gmail.com>
2023-03-03 21:32:30 -0800

    x
    x

--- next-testing/src/pages/index.js
+++ next-testing/src/pages/index.js
@@ -1,5 +1,3 @@
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import HomePage from "@/components/HomePage";
 export default function Home() {
   return <HomePage />;
