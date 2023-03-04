a28bb4bca3d1751aaf9b8af57f47396cf90f8394
alexxygg <alexxygg@gmail.com>
2023-03-04 12:21:47 -0800

    Create [id].js

---
+++ next-testing/src/pages/nosecurity/[id].js
@@ -0,0 +1,31 @@
+import Accounts from "@/components/Accounts";
+import Footer from "@/components/Footer";
+import Login from "@/components/Login";
+
+import { supabase } from "lib/supabaseClient";
+
+export async function getServerSideProps({ params }) {
+  const { data, error } = await supabase
+    .from("accounts")
+    .select()
+    .eq("id", params.id)
+    .single();
+
+  if (error) {
+    console.log(error);
+    return { notFound: true };
+  }
+
+  return { props: { account: data } };
+}
+
+function AccountsPage({ account }) {
+  return (
+    <>
+      <Accounts />
+      <Footer />
+    </>
+  );
+}
+
+export default AccountsPage;
